3672a57a09dd9f355e299aed484021b1938b771d
fport <furkanportakalx@gmail.com>
2022-08-17 17:06:04 +0300

    feat: add jest lib

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -6,7 +6,7 @@ export interface ButtonProps {
 }
 
 const Button = (props: ButtonProps) => {
-  return <button>{props.label}</button>;
+  return <button>{props.label}</button>
 };
 
 export default Button;
